1e7f382f060a4a10b6773620ca6ed8ca47b8f226
Pkononsky <43210059+Pkononsky@users.noreply.github.com>
2021-04-22 19:41:02 +0500

    Add files via upload

--- dz2/dz2.c
+++ dz2/dz2.c
@@ -20,9 +20,11 @@ int main(int argc, char *argv[]) {
     	return 1;
     }
 
+    // Буфер для записи в файл
     char buf_2_write[BUFFER_SIZE];
+    // Буфер для чтения из потока
     char buf_2_read[BUFFER_SIZE];
-
+    // счетчик байтов для записи
     int bytes_2_write = 0;
     int zeros = 0;
     int i = 0;
@@ -30,26 +32,29 @@ int main(int argc, char *argv[]) {
 
     while (readed = read(0, buf_2_read, BUFFER_SIZE)) {
     	while(i < readed) {
+    		// пока не встретим \0 запоминаем блок символов
     		while(buf_2_read[i] != 0 && i < readed) {
     			buf_2_write[bytes_2_write] = buf_2_read[i];
     			bytes_2_write++;
     			i++;
     		}
-
+    		// считаем все \0 после до следующего блока с текстом
     		while(buf_2_read[i] == 0 && i < readed) {
     			zeros++;
     			i++;
     		}
-
+    		// записываем блок символов (если он не пустой)
     		if (bytes_2_write != 0) {
 		    	write(fd, buf_2_write, bytes_2_write);
 		    	bytes_2_write = 0;
     		}
-
+    		// делаем lseek на количество нулей
     		if (zeros != 0) {	
     			lseek(fd, zeros, SEEK_CUR);
     			zeros = 0;
     		}
+    		// повторяем пока не закончился блок размером BUFFER_SIZE
+    		// если блок кончился читаем следующий, либо выходим
     	}
 
     	i = 0;
